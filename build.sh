60214534e39ba2836bdcae9ea3631f3e98f1db1d
ALESSANDRO |   R8S <alessandroaraujo0098@gmail.com>
2026-07-26 19:59:27 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -65,9 +65,9 @@ if [ ! -f "$CLANG_DIR/bin/clang-14" ]; then
     rm -rf $CLANG_DIR
     mkdir -p $CLANG_DIR
     pushd $CLANG_DIR > /dev/null
-    curl -LJOk https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/tags/android-13.0.0_r13/clang-r450784d.tar.gz
-    tar xf android-13.0.0_r13-clang-r450784d.tar.gz
-    rm android-13.0.0_r13-clang-r450784d.tar.gz
+    curl -LJOk https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/a71fa4c09d7109d611ee63964fc9fca58fee38cd/clang-r450784d.tar.gz
+    tar xf linux-x86-a71fa4c09d7109d611ee63964fc9fca58fee38cd-clang-r450784d.tar.gz
+    rm linux-x86-a71fa4c09d7109d611ee63964fc9fca58fee38cd-clang-r450784d.tar.gz
     echo "Cleaning up..."
     popd > /dev/null
 fi
